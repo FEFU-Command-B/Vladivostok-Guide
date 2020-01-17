ef4d315a4bf5dc22b8302d3e991e06039b01b82b
Max <max3jk@gmail.com>
2020-01-18 03:56:39 +1000

    build

---
+++ client/build/precache-manifest.9ac9d5ca86be2dc3d5f2b9fd857fdb91.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5076460056644785f33af887417e58",
+    "url": "/Vladivostok-Guide/index.html"
+  },
+  {
+    "revision": "d4ee200fb741efd3536c",
+    "url": "/Vladivostok-Guide/static/js/main.66fd8bfc.chunk.js"
+  },
+  {
+    "revision": "f95c3e05ecdbef78011f",
+    "url": "/Vladivostok-Guide/static/js/runtime-main.eea1c7de.js"
+  }
+]);
---
+++ client/build/static/js/main.66fd8bfc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],[function(n,t,c){n.exports=c(1)},function(n,t){}],[[0,1]]]);
+//# sourceMappingURL=main.66fd8bfc.chunk.js.map
